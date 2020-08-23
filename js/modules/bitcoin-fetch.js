e06e6e93163a74f9007cd003c509fb6b74f4d8cf
Paulo <pauloh16.pdr@gmail.com>
2020-08-23 02:52:52 -0300

    Refatorar bitcoin fetch

--- js/modules/bitcoin-fetch.js
+++ js/modules/bitcoin-fetch.js
@@ -1,8 +1,8 @@
-export default function iniciarBitcoinFetch() {
-    fetch('https://blockchain.info/ticker')
+export default function bitcoinFetch(url, alvo) {
+    fetch(url)
     .then((resposta) => resposta.json())
     .then((bitcoin) => {
-        const btcPreco = document.querySelector(".btc-preco");
+        const btcPreco = document.querySelector(alvo);
         const precoDoacao = (1000 / bitcoin.BRL.sell).toFixed(4);
         btcPreco.innerText = String(precoDoacao).replace(".", ",");
     }).catch((erro) => {
