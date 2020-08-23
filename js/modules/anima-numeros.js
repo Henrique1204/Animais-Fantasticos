26762991c948608514725665fbb58243cee96c34
Paulo <pauloh16.pdr@gmail.com>
2020-08-22 23:22:53 -0300

    Transformar animar numeros em classe

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,36 +1,52 @@
-export default function iniciarAnimaNumeros() {
-    const numeros = document.querySelectorAll("[data-numero]");
+export default class AnimaNumeros {
+    constructor(numeros, observadorAlvo, observadorClasse) {
+        this.numeros = document.querySelectorAll(numeros);
+        this.observadorAlvo = document.querySelector(observadorAlvo);
+        this.observadorClasse = observadorClasse;
 
-    function animarNumeros() {
-        if (numeros.length) {
-            numeros.forEach((numero) => {
-                const total = +numero.innerText;
-                const incremento = Math.floor(total / 100);
+        this.manipularMutacao = this.manipularMutacao.bind(this);
+    }
 
-                let inicio = 0;
-                const intervalor = setInterval(() => {
-                    inicio += incremento;
+    static incrementarNumeros(numero) {
+        const total = +numero.innerText;
+        const incremento = Math.floor(total / 100);
 
-                    numero.innerText = inicio;
+        let inicio = 0;
+        const intervalor = setInterval(() => {
+            inicio += incremento;
 
-                    if (inicio > total) {
-                        numero.innerText = total;
-                        clearInterval(intervalor);
-                    }
-                }, 35 * Math.random());
-            });
+            numero.innerText = inicio;
+
+            if (inicio > total) {
+                numero.innerText = total;
+                clearInterval(intervalor);
+            }
+        }, 35 * Math.random());
+    }
+
+    animarNumeros() {
+        if (this.numeros.length) {
+            this.numeros.forEach((numero) => this.constructor.incrementarNumeros(numero));
         }
     }
 
-    let observador;
-
-    function manipularMutacao(mutacao) {
-        if (mutacao[0].target.classList.contains("ativo")) {
-            observador.disconnect();
-            animarNumeros();
+    manipularMutacao(mutacao) {
+        if (mutacao[0].target.classList.contains(this.observadorClasse)) {
+            this.observador.disconnect();
+            this.animarNumeros();
         }
     }
 
-    observador = new MutationObserver(manipularMutacao);
-    observador.observe(document.querySelector("#numeros"), { attributes: true });
+    addObservarMutacao() {
+        this.observador = new MutationObserver(this.manipularMutacao);
+        this.observador.observe(this.observadorAlvo, { attributes: true });
+    }
+
+    iniciar() {
+        if (this.numeros.length && this.observadorAlvo) {
+            this.addObservarMutacao();
+        }
+
+        return this;
+    }
 }
