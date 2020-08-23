a77301279eed3532ed46fccac198fc8c6caf5277
Paulo <pauloh16.pdr@gmail.com>
2020-08-23 09:12:20 -0300

    Transformar horario funcionamento em classe

--- js/modules/horario-funcionamento.js
+++ js/modules/horario-funcionamento.js
@@ -1,23 +1,46 @@
-export default function iniciarHoraripFunciomente() {
-    // Dados do html
-    const funcionamento = document.querySelector("[data-semana]");
-    const diasSemanas = funcionamento.dataset.semana.split(",").map(Number);
-    const horarios = funcionamento.dataset.horario.split(",").map(Number);
-    // Dados de controle
-    const dataAgora = new Date();
-    const diaAgora = dataAgora.getDay();
-    const horaAgora = dataAgora.getHours();
-    const diaAberto = diasSemanas.includes(diaAgora);
-    const horarioAberto = (horaAgora >= horarios[0] && horaAgora < horarios[1]);
+export default class HorarioFuncionamento {
+    constructor(funcionamento) {
+        this.funcionamento = document.querySelector(funcionamento);
+    }
 
-    if (diaAberto && horarioAberto) {
-        funcionamento.classList.remove("fechado");
-        funcionamento.classList.add("aberto");
-        funcionamento.setAttribute("aria-label", "Aberto");
-    } else {
-        funcionamento.classList.remove("aberto");
-        funcionamento.classList.add("fechado");
-        funcionamento.setAttribute("aria-label", "Fechado");
+    dadosFuncionamento() {
+        this.diasSemanas = this.funcionamento.dataset.semana.split(",").map(Number);
+        this.horarios = this.funcionamento.dataset.horario.split(",").map(Number);
+    }
+
+    dadosAgora() {
+        this.dataAgora = new Date();
+        this.diaAgora = this.dataAgora.getDay();
+        this.horaAgora = this.dataAgora.getUTCHours() - 3;
+    }
+
+    estaAberto() {
+        const diaAberto = this.diasSemanas.includes(this.diaAgora);
+        const horarioAberto = (this.horaAgora >= this.horarios[0] && this.horaAgora < this.horarios[1]);
+
+        return diaAberto && horarioAberto;
+    }
+
+    ativarAberto() {
+        if (this.estaAberto()) {
+            this.funcionamento.classList.remove("fechado");
+            this.funcionamento.classList.add("aberto");
+            this.funcionamento.setAttribute("aria-label", "Aberto");
+        } else {
+            this.funcionamento.classList.remove("aberto");
+            this.funcionamento.classList.add("fechado");
+            this.funcionamento.setAttribute("aria-label", "Fechado");
+        }
+    }
+
+    iniciar() {
+        if (this.funcionamento) {
+            this.dadosAgora();
+            this.dadosFuncionamento();
+            this.ativarAberto();
+        }
+
+        return this;
     }
 }
 
